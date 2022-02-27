81dbc144c0f45d37d2479323f3def8d8865154aa
Louvet Thomas <thomas.louvet@laposte.net>
2022-02-27 19:22:42 +0100

    fixing Dashboard Bug

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -134,21 +134,20 @@ export default class {
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
     if (this.counter % 2 === 0) {
+      const fBills = filteredBills(bills, getStatus(this.index))
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
       $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
+        .html(cards(fBills))
       this.counter ++
+      fBills.forEach(bill => {
+        $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      })
     } else {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
       $(`#status-bills-container${this.index}`)
         .html("")
       this.counter ++
     }
-
-    bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
-    })
-
     return bills
 
   }
